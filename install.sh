9b2e92ddc8dbc74e84d646d1cc8f6f1d0718fbb8
kirre02 <kirre02@outlook.com>
2023-08-19 14:52:16 +0200

    feat: added a install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Set the name of your Go binary
+BINARY_NAME="net-cli"
+
+# Build the Go binary
+go build -o "${BINARY_NAME}"
+
+# Remove the current binary if it exists
+if [ -f "/usr/local/bin/${BINARY_NAME}" ]; then
+    echo "Previous installation found. Removing the existing binary..."
+    sudo rm "/usr/local/bin/${BINARY_NAME}"
+fi
+
+# Move the new binary to /usr/local/bin
+sudo mv "./${BINARY_NAME}" "/usr/local/bin"
+
+echo "Installation complete! The binary has been updated."
